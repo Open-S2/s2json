d06216e047abadef3c5dbbdbeadbaf33b22a460b
Mr Martian <craiggles@s2maps.io>
2025-02-23 19:02:43 -0500

    improve testing of types

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,5 +1,6 @@
-import { VectorPoint } from '../src';
 import { expect, test } from 'bun:test';
+
+import type { BBOX, BBox, BBox3D, MValue, Properties, VectorPoint } from '../src';
 
 test('VectorPoint', () => {
   const p: VectorPoint = { x: 1, y: 2, z: 3, m: { a: 1, b: 'two' }, t: 5 };
@@ -8,4 +9,31 @@ test('VectorPoint', () => {
   expect(p.z).toEqual(3);
   expect(p.m).toEqual({ a: 1, b: 'two' });
   expect(p.t).toEqual(5);
+});
+
+test('BBox', () => {
+  const b: BBox = [1, 2, 3, 4];
+  expect(b).toEqual([1, 2, 3, 4]);
+});
+
+test('BBox3D', () => {
+  const b: BBox3D = [1, 2, 3, 4, 5, 6];
+  expect(b).toEqual([1, 2, 3, 4, 5, 6]);
+});
+
+test('MValue', () => {
+  const m: MValue = { a: 1, b: 'two' };
+  expect(m).toEqual({ a: 1, b: 'two' });
+});
+
+test('Properties', () => {
+  const p: Properties = { a: 1, b: 'two' };
+  expect(p).toEqual({ a: 1, b: 'two' });
+});
+
+test('BBOX', () => {
+  const b: BBOX = [1, 2, 3, 4];
+  expect(b).toEqual([1, 2, 3, 4]);
+  const b2: BBOX = [1, 2, 3, 4, 5, 6];
+  expect(b2).toEqual([1, 2, 3, 4, 5, 6]);
 });
